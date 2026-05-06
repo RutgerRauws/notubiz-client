7b2814f2416b5f26f358e6ce415fce6477bac686
Rutger Rauws <rutgerrauws@gmail.com>
2026-05-06 18:30:27 +0200

    Add an example of getting a module

--- examples/get-module.ts
+++ examples/get-module.ts
@@ -1,7 +1,17 @@
 import { createClient } from './client'
-import { formatDate } from '../src/utils/utils'
 
 async function main(): Promise<void> {
+  //await listAllModules()
+  //await fetchMotions()
+  await fetchCouncilProposals()
+}
+
+void main().catch((error: unknown) => {
+  console.error(error)
+  process.exitCode = 1
+})
+
+async function listAllModules(): Promise<void> {
   const client = createClient()
   const modules = await client.modules.getAll()
 
@@ -10,7 +20,39 @@ async function main(): Promise<void> {
   }
 }
 
-void main().catch((error: unknown) => {
-  console.error(error)
-  process.exitCode = 1
-})
+async function fetchMotions(): Promise<void> {
+  const client = createClient()
+  const modules = await client.modules.getAll()
+
+  const motionsModuleId = modules.findByName('Moties')?.id
+  if (motionsModuleId === undefined) {
+    console.error('Motions module not found.')
+    return
+  }
+
+  const startDate = new Date(2025, 1, 1)
+  const endDate = new Date(2025, 2, 1)
+
+  const motionsModule = await client.modules.getById(
+    motionsModuleId,
+    startDate,
+    endDate
+  )
+  console.log(motionsModule)
+}
+
+async function fetchCouncilProposals(): Promise<void> {
+  const client = createClient()
+
+  const startDate = new Date(2025, 1, 1)
+  const endDate = new Date(2025, 2, 1)
+
+  const proposals = await client.modules.getById(19, startDate, endDate) //For Eindhoven this contains the council proposals
+
+  for (const proposal of proposals) {
+    console.log(proposal.name)
+    for (const attribute of proposal.attributes) {
+      console.log(`- ${attribute.label}: ${attribute.value}`)
+    }
+  }
+}
